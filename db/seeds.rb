a2933a31d17f7e99d9e57c810105e52c418417fd
alfmunny <alfmunny@gmail.com>
2020-11-14 22:33:43 +0100

    Finish user signup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: "User 1", email: "alfmunny@hotmail.com", password: "foobar", password_confirmation: "foobar")
